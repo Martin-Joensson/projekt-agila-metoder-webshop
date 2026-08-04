24e4d8a1533e5362d0a203fb0439519693e5193a
f0jzd <tomassavela@gmail.com>
2026-08-04 14:47:34 +0200

    Added a font, imported the font, added a component for the banner, added the banner in the page for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Banner from "./components/banner";
 import type { ProductsResponse } from "./types";
 
 const API_URL = "http://localhost:4000";
@@ -17,6 +18,10 @@ console.log(products);
 
   return (
     <main>
+
+      <Banner/>
+
+
       <h1>Products</h1>
       <div>{products.map((product) => <h2 key={product.id}>{product.title} - {product.category?.name}</h2>)}</div>
     </main>
