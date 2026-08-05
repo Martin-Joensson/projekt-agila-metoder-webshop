1e37b424212109424b993458eacc7c207a626771
Martin Jönsson <martin.t.joensson@gmail.com>
2026-08-05 12:28:58 +0200

    Fix: Changed colors and spacing to better reflect design.

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,23 +1,23 @@
 export const SearchBar = () => {
   return (
-    <div className="flex bg-gray-100 rounded-lg p-4 my-8 gap-4">
+    <div className="flex flex-col sm:flex-row bg-gray-100 rounded-lg p-4 my-8 gap-4">
       <input
         placeholder="Search Products..."
-        className="flex-1 p-2 border rounded-md"
+        className="flex-2 p-2 border border-gray-300  rounded-md"
       ></input>
 
-      <div className="flex gap-4">
+      <div className="flex flex-1 gap-4">
         {/* Filter on category */}
-        <select className="p-2 border rounded-md">
+        <select className="flex-1 p-2 border border-gray-300 rounded-md">
           <option>All categories</option>
         </select>
 
         {/* Filter on stock */}
-        <select className="p-2 border rounded-md">
+        <select className="flex-1 p-2 border border-gray-300  rounded-md">
           <option>All Stock</option>
         </select>
 
-        <button className="p-2 border rounded-md hover:bg-gray-200 active:bg-gray-300 active:translate-y-px">
+        <button className="flex items-center gap-1 p-2 border border-gray-300  rounded-md hover:bg-gray-200 active:bg-gray-300 active:translate-y-px">
           <span className="material-symbols material-symbols-filled ">
             Filter_alt
           </span>
