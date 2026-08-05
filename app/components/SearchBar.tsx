84d1f171aa548fe52277998e05e600925ef227c6
Martin Jönsson <martin.t.joensson@gmail.com>
2026-08-05 16:04:48 +0200

    Fix: Updated to more semantic tags. Added type to button.

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 export const SearchBar = () => {
   return (
-    <div className="flex flex-col sm:flex-row bg-gray-100 rounded-lg p-5 my-8 gap-4">
+    <form className="flex flex-col sm:flex-row bg-gray-100 rounded-lg p-5 my-8 gap-4">
       <input
         placeholder="Search products..."
         className="flex-2 px-3 py-2 border border-gray-300  rounded-md"
@@ -17,13 +17,13 @@ export const SearchBar = () => {
           <option>All Stock</option>
         </select>
 
-        <button className="flex items-center gap-1 p-2 border border-gray-300  rounded-md hover:bg-gray-200 active:bg-gray-300 active:translate-y-px">
+        <button type="submit" className="flex items-center gap-1 p-2 border border-gray-300  rounded-md hover:bg-gray-200 active:bg-gray-300 active:translate-y-px">
           <span className="material-symbols material-symbols-filled ">
             Filter_alt
           </span>
           <span>Filter</span>
         </button>
       </div>
-    </div>
+    </form>
   );
 };
