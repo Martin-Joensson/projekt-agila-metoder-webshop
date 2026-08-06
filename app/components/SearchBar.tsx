c5aeecbb4b00a8ad7fcd849b7ad721cd6615f49f
Martin Jönsson <martin.t.joensson@gmail.com>
2026-08-06 08:46:58 +0200

    Fix: Added missing label to input. Changed labeltext to reflect what the label is pointing to.

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,15 +1,18 @@
 export const SearchBar = () => {
   return (
-    <form className="flex flex-col sm:flex-row bg-gray-100 rounded-lg p-5 my-8 gap-4">
+    <form className="flex items-center flex-col sm:flex-row bg-gray-100 rounded-lg p-5 my-8 gap-4">
+      <label htmlFor="search-product" className="sr-only">
+        Search for product:
+      </label>
       <input
+        id="search-product"
         placeholder="Search products..."
         className="flex-2 px-3 py-2 border border-gray-300  rounded-md"
-      ></input>
+      />
 
       <div className="flex flex-1 gap-4">
         {/* Filter on category */}
         <label htmlFor="category-filter" className="sr-only">
-          {" "}
           Filter by category:
         </label>
         <select
@@ -21,8 +24,7 @@ export const SearchBar = () => {
 
         {/* Filter on stock */}
         <label htmlFor="stock-filter" className="sr-only">
-          {" "}
-          Filter by category:
+          Filter by stock-availability:
         </label>
         <select
           id="stock-filter"
