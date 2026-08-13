408b8732fd0d27bd9030adc79bef6f59e4465203
f0jzd <tomassavela@gmail.com>
2026-08-13 11:38:27 +0200

    Feature: testing a form

--- app/add-product/page.tsx
+++ app/add-product/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+
+import { Product } from "@/types";
+import { useState } from "react";
+
+interface FormData {
+  text1: string;
+  text2: string;
+  text3: string;
+  text4: string;
+  numberInput: number;
+  enumDropdown: FormDataEnum;
+}
+
+enum FormDataEnum {
+  Opt1 = "Opt1",
+  Opt2 = "Opt2",
+  Opt3 = "Opt3",
+}
+
 export default function AddProductPage() {
+  const [formData, setFormData] = useState<FormData>({
+    text1: "",
+    text2: "",
+    text3: "",
+    text4: "",
+    numberInput: 0,
+    enumDropdown: FormDataEnum.Opt1,
+  });
+
   return (
     <main>
-      <div>Add Product Design goes here.</div>
+      <form action=""></form>
     </main>
   );
 }
