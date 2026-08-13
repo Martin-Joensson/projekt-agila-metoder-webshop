710ed715da66524f55a81d27fac27b253e5782b3
f0jzd <tomassavela@gmail.com>
2026-08-13 12:57:28 +0200

    Feature: temporary form without functionality

--- app/add-product/page.tsx
+++ app/add-product/page.tsx
@@ -3,34 +3,88 @@
 import { Product } from "@/types";
 import { useState } from "react";
 
-interface FormData {
-  text1: string;
-  text2: string;
-  text3: string;
-  text4: string;
-  numberInput: number;
-  enumDropdown: FormDataEnum;
-}
-
-enum FormDataEnum {
-  Opt1 = "Opt1",
-  Opt2 = "Opt2",
-  Opt3 = "Opt3",
-}
-
 export default function AddProductPage() {
-  const [formData, setFormData] = useState<FormData>({
-    text1: "",
-    text2: "",
-    text3: "",
-    text4: "",
-    numberInput: 0,
-    enumDropdown: FormDataEnum.Opt1,
+  const handleSubmit = (e: React.SubmitEvent) => {
+    e.preventDefault();
+    console.log("Form Data Submitted:", formData);
+    // Add your API call or further logic here
+  };
+
+  const [formData, setFormData] = useState<Product>({
+    id: 0,
+    title: "",
+    description: "",
+    thumbnail: "",
+    brand: "",
+    price: 0,
+    categoryId: 0,
+    meta: {
+      createdAt: "",
+      updatedAt: "",
+      barcode: "",
+      qrCode: "",
+    },
+    images: [],
   });
 
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
-    <main>
-      <form action=""></form>
-    </main>
+    <div style={{ maxWidth: "400px", margin: "auto" }}>
+      <input
+        type="text"
+        name="title"
+        value={formData.title}
+        onChange={handleChange}
+        placeholder="Enter text"
+      />
+      <input
+        type="text"
+        name="description"
+        value={formData.description}
+        onChange={handleChange}
+        placeholder="Enter description"
+      />
+      <input
+        type="text"
+        name="thumbnail"
+        value={formData.thumbnail}
+        onChange={handleChange}
+        placeholder="Enter thumbnail"
+      />
+      <input
+        type="text"
+        name="brand"
+        value={formData.brand}
+        onChange={handleChange}
+        placeholder="Enter brand"
+      />
+      <input
+        type="number"
+        name="price"
+        value={formData.price}
+        onChange={handleChange}
+        placeholder="Enter price"
+      />
+      <select
+        name="categoryId"
+        value={formData.categoryId}
+        onChange={handleChange}
+      >
+        <option value={1}>Test</option>
+        <option value={2}>Test2</option>
+        <option value={3}>Test3</option>
+        <option value={4}>Test4</option>
+      </select>
+    </div>
   );
 }
