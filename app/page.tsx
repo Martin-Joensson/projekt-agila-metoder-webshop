c889692749f90e8e1c42a7fe9d0cc5d935b7fb3e
Martin Jönsson <martin.t.joensson@gmail.com>
2026-08-20 12:11:42 +0200

    Add: Dynamic stock numbers.

--- app/page.tsx
+++ app/page.tsx
@@ -12,16 +12,20 @@ export default async function Home() {
   // we also use _expand to get the relational category data
   // we can use the other destructed variables like page, total and so on to create pagination or show info
   const { products, total, page, pages, limit }: ProductsResponse = await fetch(
-    `${API_URL}/products/?_limit=${defaultLimit}&_sort=id&_order=desc&_expand=category`,
+    `${API_URL}/products/?_limit=${defaultLimit}_sort=id&_order=desc&_expand=category`,
   ).then((res) => res.json());
+
+  const inStock = products.filter((product) => product.stock >= 10);
+  const lowStock = products.filter((product) => product.stock < 10);
+  const outOfStock = products.filter((product) => product.stock === 0);
 
   return (
     <main className="max-w-7xl w-full mx-auto p-4 flex flex-col gap-4">
       <div className="flex flex-col sm:flex-row gap-2">
-        <FilterCard category="products" value={1203} />
-        <FilterCard category="instock" value={123} />
-        <FilterCard category="lowstock" value={33} />
-        <FilterCard category="outofstock" value={3} />
+        <FilterCard category="products" value={products.length} />
+        <FilterCard category="instock" value={inStock.length} />
+        <FilterCard category="lowstock" value={lowStock.length} />
+        <FilterCard category="outofstock" value={outOfStock.length} />
       </div>
       <SearchBar />
       <ProductList
