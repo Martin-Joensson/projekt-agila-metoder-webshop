6dbbae6d7df849f3c032e816b37a1b2095e7bb1d
josie <160166451+josiefinis@users.noreply.github.com>
2026-08-26 07:37:27 +0200

    add: show message on invalid search input

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,25 +1,35 @@
 "use client";
 import { debounce, isValidRegExp } from "@/lib/utils";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 export default function Search() {
   const searchParams = useSearchParams();
   const path = usePathname();
   const { replace } = useRouter();
+  const [message, setMessage] = useState("");
 
   const handleSearch = debounce((term: string) => {
     const params = new URLSearchParams(searchParams);
-    if (term.length > 2 && isValidRegExp(term)) {
+    const validation = validate(term);
+    if (validation.success) {
       params.delete("page");
       params.set("search", term);
     } else {
       params.delete("search");
     }
+
+    setMessage(validation.message);
     replace(`${path}?${params.toString()}`, { scroll: false });
   }, 300);
 
   return (
-    <>
+    <div className="relative w-full sm:w-auto flex-1">
+      {message && (
+        <p className="absolute inset-y-10 inset-x-3 font-semibold text-red-600 text-sm">
+          {message}
+        </p>
+      )}
       <label htmlFor="search-product" className="sr-only">
         Search for product:
       </label>
@@ -27,12 +37,29 @@ export default function Search() {
         id="search-product"
         type="search"
         placeholder="Search products..."
-        className="w-full sm:w-auto flex-1 px-3 py-2 border border-gray-300 rounded-md"
+        className="w-full px-3 py-2 border border-gray-300 rounded-md"
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
         defaultValue={searchParams.get("search")?.toString()}
       />
-    </>
+    </div>
   );
+}
+
+function validate(term: string): { success: boolean; message: string } {
+  if (!term) {
+    return { success: false, message: "" };
+  }
+  if (term.length < 3) {
+    return { success: false, message: "Enter at least 3 characters." };
+  }
+  if (!isValidRegExp(term)) {
+    return {
+      success: false,
+      message:
+        "Enter a valid regular expression or avoid using special characters.",
+    };
+  }
+  return { success: true, message: "" };
 }
